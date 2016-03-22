d7d3ba94be734b68c3f8c5a026de9412ea0bf4c7
abradle <anthony.richard.bradley@gmail.com>
2016-03-22 11:52:03 -0700

    Update of the structure decoder interface to allow preparation of data model. Prepare takes the number of models, chains, residues and atoms.

--- mmtf-api/src/main/java/org/rcsb/mmtf/api/StructureDecoderInterface.java
+++ mmtf-api/src/main/java/org/rcsb/mmtf/api/StructureDecoderInterface.java
@@ -113,13 +113,20 @@ public interface StructureDecoderInterface {
   void setInterGroupBonds(int thisBondIndOne,
       int thisBondIndTwo, int thisBondOrder);
   
+  
+  /**
+   * A generic function to be used before any additions to do any required pre-processing.
+   * For example the user could use this to specify the amount of memory to be allocated.
+   * @param totalNumAtoms The total number of atoms found in the data.
+   * @param totalNumGroups The total number of groups found in the data.
+   * @param totalNumChains The total number of chains found in the data.
+   * @param totalNumModels The total number of models found in the data.
+   */
+  void prepareStructure(int totalNumAtoms, int totalNumGroups, int totalNumChains, int totalNumModels);
+  
   /**
    * A generic function to be used at the end of all data addition to do required cleanup on the structure
    */
   void cleanUpStructure();
   
-  /**
-   * A generic function to be used before any additions to do any required pre-processing.
-   */
-  void prepareStructure();
 }
