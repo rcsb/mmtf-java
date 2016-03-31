73165a4e311670edb2a1d9e7e1c374c629a9917c
Anthony Bradley <anthony.richard.bradley@gmail.com>
2016-03-31 08:20:30 -0700

    Uncomment tests

--- mmtf-update/src/main/java/org/rcsb/mmtf/update/IntegrationTestUtils.java
+++ mmtf-update/src/main/java/org/rcsb/mmtf/update/IntegrationTestUtils.java
@@ -10,30 +10,30 @@ public class IntegrationTestUtils {
 	public static final String[] TEST_CASES = new String[] {
 			//Standard structure
 			"4cup",
-//			// Weird NMR structure
-//			"1o2f",
-//			// Another weird structure (jose's suggestion) 
-//			"3zyb",
-//			// B-DNA structure
-//			"1bna", 
-//			// DNA structure
-//			"4y60",
-//			// Sugar structure
-//			"1skm",
-//			// Ribosome
-//			"4v5a",
-//			// Biosynthetic protein
-//			"5emg",
-//			// Calpha atom is missing (not marked as calpha)
-//			"1lpv",
-//			// NMR structure with multiple models - one of which has chain missing
-//			"1msh",
-//			// No ATOM records just HETATM records (in PDB). Opposite true for MMCif. It's a D-Peptide.
-//			"1r9v",
-//			// Micro heterogenity
-//			"4ck4",
-//			// Negative residue numbers
-//			"5esw",
+			// Weird NMR structure
+			"1o2f",
+			// Another weird structure (jose's suggestion) 
+			"3zyb",
+			// B-DNA structure
+			"1bna", 
+			// DNA structure
+			"4y60",
+			// Sugar structure
+			"1skm",
+			// Ribosome
+			"4v5a",
+			// Biosynthetic protein
+			"5emg",
+			// Calpha atom is missing (not marked as calpha)
+			"1lpv",
+			// NMR structure with multiple models - one of which has chain missing
+			"1msh",
+			// No ATOM records just HETATM records (in PDB). Opposite true for MMCif. It's a D-Peptide.
+			"1r9v",
+			// Micro heterogenity
+			"4ck4",
+			// Negative residue numbers
+			"5esw",
 			// A weird case with incorrect residue numbers.
 	//		"3th3"
 			};
