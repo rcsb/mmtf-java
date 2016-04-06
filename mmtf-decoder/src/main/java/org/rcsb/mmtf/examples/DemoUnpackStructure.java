ffdc19313cf4c91fd162026155457791688ca9a4
Anthony Bradley <anthony.richard.bradley@gmail.com>
2016-04-05 19:17:26 -0700

    Some refactoring of the demo

--- mmtf-decoder/src/main/java/org/rcsb/mmtf/examples/DemoUnpackStructure.java
+++ mmtf-decoder/src/main/java/org/rcsb/mmtf/examples/DemoUnpackStructure.java
@@ -9,31 +9,38 @@ import org.rcsb.mmtf.api.MmtfDecodedDataInterface;
  */
 public class DemoUnpackStructure {
 
-
+	/**
+	 * Main function to run the demo class
+	 * @param args no args required
+	 */
 	public static void main(String[] args) {
 		// Create an instance of the handle IO class
 		HandleIO handleIO = new HandleIO();
 		// The index of a given chain
 		int chainIndex = 0;
 		int groupIndex = 0;
-		int atomIndex = 0;
-		// Create the instance of this API
+		int structureAtomIndex = 0;
+		// Create the instance of this API - in this case for the 4cup structure's data.
 		MmtfDecodedDataInterface dataApi = handleIO.getDataApiFromUrlOrFile("4cup");
 		// Now loop through the models
 		for (int modelIndex=0; modelIndex<dataApi.getNumModels(); modelIndex++) {
-			for (int chainCounter=0; chainCounter<dataApi.getChainsPerModel()[modelIndex]; chainCounter++){
-				for (int groupCounter=0; groupCounter<dataApi.getGroupsPerChain()[chainIndex]; groupCounter++){
+			// Loop through the chains in this model
+			for (int modelChainCounter=0; modelChainCounter<dataApi.getChainsPerModel()[modelIndex]; modelChainCounter++){
+				// Loop through the groups in this chain
+				for (int chainGroupCounter=0; chainGroupCounter<dataApi.getGroupsPerChain()[chainIndex]; chainGroupCounter++){
+					// Get the indices mapping the groups to their types
 					int currentGroupTypeIndex = dataApi.getGroupTypeIndices()[groupIndex];
-					for (int atomCounter=0; atomCounter<dataApi.getNumAtomsInGroup(currentGroupTypeIndex); atomCounter++) {
-						// Use the atomIndex to get total structure info.
-						float xCoord = dataApi.getxCoords()[atomIndex];
-						int serialId = dataApi.getAtomIds()[atomIndex];
-						float bFactor = dataApi.getbFactors()[atomIndex];
+					// Loop through the atoms in every group
+					for (int groupAtomIndex=0; groupAtomIndex<dataApi.getNumAtomsInGroup(currentGroupTypeIndex); groupAtomIndex++) {
+						// Use the structureAtomIndex to get total structure info.
+						float xCoord = dataApi.getxCoords()[structureAtomIndex];
+						int serialId = dataApi.getAtomIds()[structureAtomIndex];
+						float bFactor = dataApi.getbFactors()[structureAtomIndex];
 						// Use the atomCounter to get group level info
-						String element = dataApi.getGroupElementNames(currentGroupTypeIndex)[atomCounter];
+						String element = dataApi.getGroupElementNames(currentGroupTypeIndex)[groupAtomIndex];
 						System.out.println("Serial id: "+serialId+" X coordinate: "+xCoord+", B-factor: "+bFactor+", element: "+element);
 						// Incremenet the atom index
-						atomIndex++;
+						structureAtomIndex++;
 					}
 					// Increment the groupIndex
 					groupIndex++;
@@ -42,6 +49,5 @@ public class DemoUnpackStructure {
 				chainIndex++;
 			}
 		}
-
 	}
 }
