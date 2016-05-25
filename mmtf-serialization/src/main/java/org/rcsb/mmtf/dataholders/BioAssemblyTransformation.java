eb53a78a6c7aeb2418a06e210b99f4a9cc2c51b4
Anthony Bradley <anthony.richard.bradley@gmail.com>
2016-05-25 10:36:27 -0700

    All data holders now implement serializable (for spark usage).

--- mmtf-serialization/src/main/java/org/rcsb/mmtf/dataholders/BioAssemblyTransformation.java
+++ mmtf-serialization/src/main/java/org/rcsb/mmtf/dataholders/BioAssemblyTransformation.java
@@ -1,5 +1,6 @@
 package org.rcsb.mmtf.dataholders;
 
+import java.io.Serializable;
 
 /**
  * The transformation needed for generation of biological assemblies
@@ -9,50 +10,53 @@ package org.rcsb.mmtf.dataholders;
  *
  * @author Anthony Bradley
  */
-public class BioAssemblyTransformation {
+public class BioAssemblyTransformation implements Serializable {
 
 
-  /** The indices of the chains this bioassembly references. */
-  private int[] chainIndexList;
+	/** Serial id for this version of the format. */
+	private static final long serialVersionUID = -3874846890822791985L;
 
-  /** The 4x4 matrix transformation specifying a rotation and a translation. */
-  private double[] matrix;
+	/** The indices of the chains this bioassembly references. */
+	private int[] chainIndexList;
+
+	/** The 4x4 matrix transformation specifying a rotation and a translation. */
+	private double[] matrix;
 
 
-  /**
-   * Gets the 4x4 matrix transformation specifying a rotation and a translation.
-   * FIXME is this row-packed or column-packed?
-   * @return the transformation
-   */
-  public double[] getMatrix() {
-    return matrix;
-  }
+	/**
+	 * Gets the 4x4 matrix transformation specifying a rotation and a translation.
+	 * FIXME is this row-packed or column-packed?
+	 * @return the transformation
+	 */
+	public double[] getMatrix() {
+		return matrix;
+	}
 
-  /**
-   * Sets the 4x4 matrix transformation specifying a rotation and a translation.
-   * FIXME is this row-packed or column-packed?
-   * @param transformation the new transformation
-   */
-  public void setMatrix(double[] transformation) {
-    this.matrix = transformation;
-  }
+	/**
+	 * Sets the 4x4 matrix transformation specifying a rotation and a translation.
+	 * FIXME is this row-packed or column-packed?
+	 * @param transformation the new transformation
+	 */
+	public void setMatrix(double[] transformation) {
+		this.matrix = transformation;
+	}
 
-  /**
-   * Gets the indices of the chains this bioassembly refers to.
-   *
-   * @return a list of integers indicating the indices (zero indexed) of the chains this bioassembly refers to.
-   */
-  public int[] getChainIndexList() {
-    return chainIndexList;
-  }
+	/**
+	 * Gets the indices of the chains this bioassembly refers to.
+	 *
+	 * @return a list of integers indicating the indices (zero indexed) of the chains this bioassembly refers to.
+	 */
+	public int[] getChainIndexList() {
+		return chainIndexList;
+	}
 
-  /**
-   * Sets the chain id.
-   *
-   * @param  inputChainId a list of integers indicating the indices (zero indexed) of the chains this bioassembly refers to.
-   */
-  public void setChainIndexList(int[] inputChainId) {
-    this.chainIndexList = inputChainId;
-  }
+	/**
+	 * Sets the chain id.
+	 *
+	 * @param  inputChainId a list of integers indicating the indices (zero indexed) of the chains this bioassembly refers to.
+	 */
+	public void setChainIndexList(int[] inputChainId) {
+		this.chainIndexList = inputChainId;
+	}
 
 }
